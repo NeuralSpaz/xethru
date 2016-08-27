8cb430f5fa3f6932b95c4be89558ad164688f090
NeuralSpaz <josh@zool.com>
2016-08-28 06:06:17 +1000

    godoc stuff

--- x2m200_test.go
+++ x2m200_test.go
@@ -1,18 +1,12 @@
-// This file is part of Xethru-Go - A Golang library for the xethru modules
-//
-// The MIT License (MIT)
 // Copyright (c) 2016 Josh Gardiner aka NeuralSpaz on github.com
-
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-
 // The above copyright notice and this permission notice shall be included
 // in all copies or substantial portions of the Software.
-
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,6 +14,8 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
+// Xethru-GO a driver for the xethru sensor modules
 package xethru
 
 import (
@@ -66,7 +62,7 @@ func x2m200ProtocolwithTransit(in []byte) ([]byte, []byte, error) {
 	return readback, transit, err
 }
 
-func TestXethruWrite(t *testing.T) {
+func TestX2M200Write(t *testing.T) {
 
 	cases := []struct {
 		b      []byte
@@ -103,7 +99,7 @@ func TestXethruWrite(t *testing.T) {
 	}
 }
 
-func TestXethruRead(t *testing.T) {
+func TestX2M200Read(t *testing.T) {
 
 	cases := []struct {
 		readback []byte
